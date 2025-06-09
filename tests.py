27a9af223a3a4310c79f4b11c092ee84eccb91c3
Lukas Lazarek <LukasALazarek@gmail.com>
2025-06-09 17:31:39 -0400

    Add class code today

--- tests.py
+++ tests.py
@@ -1,9 +1,40 @@
 import unittest
 
-from simple import *
+from arith import *
 
 class TestInterp(unittest.TestCase):
     def test_thing(self):
+        self.assertEqual(interp(Num(5), {}),
+                         Num(5))
+    def test_thing2(self):
+        self.assertEqual(interp(Add(Num(5), Num(-1)), {}),
+                         Num(4))
+    def test_thing3(self):
+        self.assertEqual(interp(Let("x", BaseType.INT,
+                                    Num(0),
+                                    Mul(Var("x"), Num(2))),
+                                {}),
+                         Num(0))
+        self.assertEqual(interp(
+            Let("f", FunType([BaseType.INT, BaseType.INT],
+                             BaseType.INT),
+                Fun(["x", "y"],
+                    [BaseType.INT, BaseType.INT],
+                    Add(Var("x"), Var("y"))),
+                App(Var("f"),
+                    [Num(5), Add(Num(2), Num(3))])),
+            {}),
+                         interp(Mul(Num(2), Num(5)), {}))
+        # self.assertEqual(interp(
+        #     Let("omega", FunType([BaseType.INT, BaseType.INT],
+        #                          BaseType.INT),
+        #         Fun(["x"], [BaseType.INT], App(Var("x"), Var("x"))),
+        #         App(Var("omega"), Var("omega")))),
+        #                  Num(0))
+
+
+        # App(Let("x", 5, Fun(["y"], Add(x, y))), 7) == 12
+        # Let("x", 100, App(Let("x", 5, Fun(["y"], Add(x, y))), 7)) == 12
 
 if __name__ == "__main__":
     unittest.main()
