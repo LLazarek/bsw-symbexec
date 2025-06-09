b4f5c7c82caddc3c84ded2b957eca15de64cc92c
Lukas Lazarek <LukasALazarek@gmail.com>
2025-06-09 17:31:05 -0400

    Add class code today

--- arith.py
+++ arith.py
@@ -37,7 +37,7 @@ class App(NamedTuple):
 class BaseType(Enum):
     INT = 1
 class FunType(NamedTuple):
-    dom: 'Type'
+    dom: list['Type']
     rng: 'Type'
 
 
@@ -65,11 +65,40 @@ class Closure(NamedTuple):
 # An Env is a dict[str, Value]
 
 def interp(e: 'Expr', env: 'Env') -> 'Value':
-    todo
+    match e:
+        case Num(n):
+            return e
+        case BiNumOp(l, r, op, op_name):
+            lv = interp(l, env)
+            rv = interp(r, env)
+            assert isinstance(lv, Num) and isinstance(rv, Num), f"Bad argument types for {op_name}: {lv} and {rv}"
+            return Num(op(lv.n, rv.n))
+        case Var(name):
+            return lookup(env, name)
+        case Let(name, ann, expr, body):
+            val = interp(expr, env)
+            return interp(body, bind(env, [name], [val]))
+        case Fun(args, types, body):
+            return Closure(e, env)
+        case App(f, args):
+            fv = interp(f, env)
+            argvs = [interp(arg, env) for arg in args]
+            assert isinstance(fv, Closure), f"Bad app, expected function, got {fv}"
+            assert len(args) == len(fv.fun.params), f"Arity error, expected {len(fv.fun.params)} arguments, got {len(args)}"
+            return interp(fv.fun.body, bind(fv.env, fv.fun.params, argvs))
+
+        case _:
+            raise Exception(f"Unknown expr {e}")
 
 
+def lookup(env: 'Env', name: str) -> 'Value':
+    if name in env:
+        return env[name]
+    else:
+        raise Exception(f"Unbound variable {name}")
 
-
+def bind(env: 'Env', names: list[str], vals: list['Value']) -> 'Env':
+    return env | dict(zip(names, vals))
 
 
 ## ---------- Symbolic execution ----------
@@ -85,14 +114,35 @@ class BinFExpr(NamedTuple):
     r: 'Formula'
     op: str
 
-class NegFExpr(NamedTuple):
-    fe: 'Formula'
-
 # A SymbEnv is an Env where values are Formulas, ie dict[str, Formula]
 
 def symb_exec(fun: 'Fun') -> 'Formula':
     todo
 
 def symb_interp(e: 'Expr', env: 'SymbEnv') -> 'Formula':
-    todo
+    match e:
+        case Num(n):
+            # return e
+        case BiNumOp(l, r, op, op_name):
+            # lv = interp(l, env)
+            # rv = interp(r, env)
+            # assert isinstance(lv, Num) and isinstance(rv, Num), f"Bad argument types for {op_name}: {lv} and {rv}"
+            # return Num(op(lv.n, rv.n))
+        case Var(name):
+            # return lookup(env, name)
+        case Let(name, ann, expr, body):
+            # val = interp(expr, env)
+            # return interp(body, bind(env, [name], [val]))
+        case Fun(args, types, body):
+            # return Closure(e, env)
+        case App(f, args):
+            # fv = interp(f, env)
+            # argvs = [interp(arg, env) for arg in args]
+            # assert isinstance(fv, Closure), f"Bad app, expected function, got {fv}"
+            # assert len(args) == len(fv.fun.params), f"Arity error, expected {len(fv.fun.params)} arguments, got {len(args)}"
+            # return interp(fv.fun.body, bind(fv.env, fv.fun.params, argvs))
+
+        case _:
+            raise Exception(f"Unknown expr {e}")
+
 
