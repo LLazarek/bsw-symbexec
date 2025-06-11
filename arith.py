5543f74377cb3d9e19d96cd5cb1fd6c32b570d1a
Lukas Lazarek <LukasALazarek@gmail.com>
2025-06-11 13:33:58 -0400

    Starter code for today

--- arith.py
+++ arith.py
@@ -1,11 +1,14 @@
 from enum import Enum
 from typing import NamedTuple, Callable
+import z3
 
 ## ---------- Language data definitions ----------
 
 # An Expr is one of
 class Num(NamedTuple):
     n: int
+    def make_z3(self, _):
+        return self.n
 
 class BiNumOp(NamedTuple): # + - /
     l: 'Expr'
@@ -31,11 +34,30 @@ class App(NamedTuple):
     args: list['Expr']
 
 
+class Bool(NamedTuple):
+    b: bool
+    def make_z3(self, _):
+        return self.b
+class BiNumCmp(NamedTuple): # < i=
+    l: 'Expr'
+    r: 'Expr'
+    op: Callable[[int, int], bool]
+    op_name: str
+class BoolOp(NamedTuple): # and or xor b=
+    l: 'Expr'
+    r: 'Expr'
+    op: Callable[[bool, bool], bool]
+    op_name: str
+
+class Assert(NamedTuple):
+    tst: 'Expr'
+
 
 
 # a Type is one of
 class BaseType(Enum):
     INT = 1
+    BOOL = 2
 class FunType(NamedTuple):
     dom: list['Type']
     rng: 'Type'
@@ -49,15 +71,31 @@ def Sub(a, b):
     return BiNumOp(a, b, lambda a,b: a-b, '-')
 def Mul(a, b):
     return BiNumOp(a, b, lambda a,b: a*b, '*')
+def Eq(a, b):
+    return BiNumCmp(a, b, lambda a,b: a==b, '=')
+def Neq(a, b):
+    return BiNumCmp(a, b, lambda a,b: a!=b, '!=')
+def Lt(a, b):
+    return BiNumCmp(a, b, lambda a,b: a<b, '<')
+def Gt(a, b):
+    return BiNumCmp(a, b, lambda a,b: a>b, '>')
+def And(a, b):
+    return BoolOp(a, b, lambda a,b: a and b, 'and')
+def Not(a):
+    return BoolOp(a, Bool(True), lambda a,b: a != b, '!=')
+def Seq(a, b):
+    return Let("__", BaseType.BOOL, a, b)
 
 
 
-
+class AssertionFailure(Exception):
+    pass
 
 
 ## ---------- Concrete execution ----------
 # A Value is one of
 # Num
+# Bool
 class Closure(NamedTuple):
     fun: Fun
     env: 'Env'
@@ -87,6 +125,25 @@ def interp(e: 'Expr', env: 'Env') -> 'Value':
             assert len(args) == len(fv.fun.params), f"Arity error, expected {len(fv.fun.params)} arguments, got {len(args)}"
             return interp(fv.fun.body, bind(fv.env, fv.fun.params, argvs))
 
+        case Bool(b):
+            return e
+        case BiNumCmp(l, r, op, op_name):
+            lv = interp(l, env)
+            rv = interp(r, env)
+            assert isinstance(lv, Num) and isinstance(rv, Num), f"Bad argument types for {op_name}: {lv} and {rv}"
+            return Bool(op(lv.n, rv.n))
+        case BoolOp(l, r, op, op_name):
+            lv = interp(l, env)
+            rv = interp(r, env)
+            assert isinstance(lv, Bool) and isinstance(rv, Bool), f"Bad argument types for {op_name}: {lv} and {rv}"
+            return Bool(op(lv.b, rv.b))
+        case Assert(tst):
+            tst_v = interp(tst, env)
+            assert isinstance(tst_v, Bool)
+            if not tst_v.b:
+                raise AssertionFailure(f"Assertion failed: {e}")
+            return tst_v
+
         case _:
             raise Exception(f"Unknown expr {e}")
 
@@ -101,47 +158,217 @@ def bind(env: 'Env', names: list[str], vals: list['Value']) -> 'Env':
     return env | dict(zip(names, vals))
 
 
-# ## ---------- Symbolic execution ----------
+## ---------- Symbolic execution ----------
 
-# # A Formula is one of
-# # Num
-# class SymVar(NamedTuple):
-#     name: str
-#     t: BaseType
+# A Formula is one of
+# Num
+# Bool
+class SymVar(NamedTuple):
+    name: str
+    t: BaseType
+    def make_z3(self, varmap):
+        if self.name not in varmap:
+            match self.t:
+                case BaseType.INT:
+                    varmap[self.name] = z3.Int(self.name)
+                case BaseType.BOOL:
+                    varmap[self.name] = z3.Bool(self.name)
+        return varmap[self.name]
 
-# class BinFExpr(NamedTuple):
-#     l: 'Formula'
-#     r: 'Formula'
-#     op: str
+class BinFExpr(NamedTuple):
+    l: 'Formula'
+    r: 'Formula'
+    op: str
+    def make_z3(self, varmap):
+        lmade = self.l.make_z3(varmap)
+        rmade = self.r.make_z3(varmap)
+        match self.op:
+            case '+':
+                return lmade + rmade
+            case '-':
+                return lmade - rmade
+            case '*':
+                return lmade * rmade
+            case '/':
+                return lmade / rmade
+            case '=':
+                return lmade == rmade
+            case '!=':
+                return lmade != rmade
+            case '<':
+                return lmade < rmade
+            case '>':
+                return lmade > rmade
+            case 'and':
+                return z3.And(lmade, rmade)
+            case 'or':
+                return z3.Or(lmade, rmade)
+            case _:
+                raise Exception(f"Unhandled smt conversion of op {op}")
 
-# # A SymbEnv is an Env where values are Formulas, ie dict[str, Formula]
+class NegFExpr(NamedTuple):
+    fe: 'Formula'
+    def make_z3(self, varmap):
+        return z3.Not(self.fe.make_z3(varmap))
 
-# def symb_exec(fun: 'Fun') -> 'Formula':
-#     todo
 
-# def symb_interp(e: 'Expr', env: 'SymbEnv') -> 'Formula':
-#     match e:
-#         case Num(n):
-#             # return e
-#         case BiNumOp(l, r, op, op_name):
-#             # lv = interp(l, env)
-#             # rv = interp(r, env)
-#             # assert isinstance(lv, Num) and isinstance(rv, Num), f"Bad argument types for {op_name}: {lv} and {rv}"
-#             # return Num(op(lv.n, rv.n))
-#         case Var(name):
-#             # return lookup(env, name)
-#         case Let(name, ann, expr, body):
-#             # val = interp(expr, env)
-#             # return interp(body, bind(env, [name], [val]))
-#         case Fun(args, types, body):
-#             # return Closure(e, env)
-#         case App(f, args):
-#             # fv = interp(f, env)
-#             # argvs = [interp(arg, env) for arg in args]
-#             # assert isinstance(fv, Closure), f"Bad app, expected function, got {fv}"
-#             # assert len(args) == len(fv.fun.params), f"Arity error, expected {len(fv.fun.params)} arguments, got {len(args)}"
-#             # return interp(fv.fun.body, bind(fv.env, fv.fun.params, argvs))
 
-#         case _:
-#             raise Exception(f"Unknown expr {e}")
+# A SymbEnv is an Env where values are Formulas, ie dict[str, Formula]
+
+# A ConstraintSet is list['Formula']
+
+# A PathCondition is a ConstraintSet
+
+# A SymbResult is a tuple of
+# 'Formula',
+# 'PathCondition',
+# list['ConstraintSet']
+
+def symb_exec_v1(fun: 'Fun') -> 'Formula':
+    for t in fun.anns:
+        assert not isinstance(t, FunType), f"Inputs can only be first-order, but got an input of type {t}"
+    symvars = [SymVar(p, t) for p, t in zip(fun.params, fun.anns)]
+    formula, pc, avs = symb_interp(fun.body, bind({}, fun.params, symvars), [], [])
+    return formula
+
+def symb_exec(fun: 'Fun') -> 'Formula':
+    for t in fun.anns:
+        assert not isinstance(t, FunType), f"Inputs can only be first-order, but got an input of type {t}"
+    symvars = [SymVar(p, t) for p, t in zip(fun.params, fun.anns)]
+    formula, pc, avs = symb_interp(fun.body, bind({}, fun.params, symvars), [], [])
+    return formula, avs
+
+
+def symb_interpN(es: list['Expr'],
+                 env: 'SymbEnv',
+                 pathcond: 'PathCondition',
+                 avs: 'ConstraintSet',
+                 k: Callable[[list['Formula'], 'PathCondition', 'ConstraintSet'],
+                             'SymbResult']) -> 'SymbResult':
+    result_formulas = []
+    for e in es:
+        formula, pathcond, avs = symb_interp(e, env, pathcond, avs)
+        result_formulas.append(formula)
+    return k(result_formulas, pathcond, avs)
+
+def symb_interp(e: 'Expr',
+                env: 'SymbEnv',
+                pathcond: 'PathCondition',
+                avs: list['ConstraintSet']) -> 'SymbResult':
+    match e:
+        case Num(n):
+            # return e
+            return e, pathcond, avs
+        case BiNumOp(l, r, op, op_name):
+            # lv = interp(l, env)
+            # rv = interp(r, env)
+            # assert isinstance(lv, Num) and isinstance(rv, Num), f"Bad argument types for {op_name}: {lv} and {rv}"
+            # return Num(op(lv.n, rv.n))
+            def k(formulas, pathcond, avs):
+                l_formula, r_formula = formulas
+                if isinstance(l_formula, Num) and isinstance(r_formula, Num):
+                    return Num(op(l_formula.n, r_formula.n)), pathcond, avs
+                else:
+                    return BinFExpr(l_formula, r_formula, op_name), pathcond, avs
+            return symb_interpN([l, r], env, pathcond, avs, k)
+        case Var(name):
+            # return lookup(env, name)
+            return lookup(env, name), pathcond, avs
+        case Let(name, ann, expr, body):
+            # val = interp(expr, env)
+            # return interp(body, bind(env, [name], [val]))
+            formula, pc2, avs2 = symb_interp(expr, env, pathcond, avs)
+            formula_result, pc3, avs3 = symb_interp(body,
+                                                    bind(env, [name], [formula]),
+                                                    pc2,
+                                                    avs2)
+            return formula_result, pc2, avs3
+        case Fun(args, types, body):
+            # return Closure(e, env)
+            return Closure(e, env), pathcond, avs
+        case App(f, args):
+            # fv = interp(f, env)
+            # argvs = [interp(arg, env) for arg in args]
+            # assert isinstance(fv, Closure), f"Bad app, expected function, got {fv}"
+            # assert len(args) == len(fv.fun.params), f"Arity error, expected {len(fv.fun.params)} arguments, got {len(args)}"
+            # return interp(fv.fun.body, bind(fv.env, fv.fun.params, argvs))
+            def k(formulas, pathcond, avs):
+                fv = formulas[0]
+                argfs = formulas[1:]
+                assert isinstance(fv, Closure), f"Bad app, expected function, got {fv}"
+                assert len(args) == len(fv.fun.params), f"Arity error, expected {len(fv.fun.params)} arguments, got {len(args)}"
+                return symb_interp(fv.fun.body,
+                                   bind(fv.env, fv.fun.params, argfs),
+                                   pathcond,
+                                   avs)
+            return symb_interpN([f, *args], env, pathcond, avs, k)
+        case Bool(b):
+            # return e
+            return e, pathcond, avs
+        case BiNumCmp(l, r, op, op_name):
+            # lv = interp(l, env)
+            # rv = interp(r, env)
+            # assert isinstance(lv, Num) and isinstance(rv, Num), f"Bad argument types for {op_name}: {lv} and {rv}"
+            # return Bool(op(lv.n, rv.n))
+            def k(formulas, pathcond, avs):
+                l_formula, r_formula = formulas
+                if isinstance(l_formula, Num) and isinstance(r_formula, Num):
+                    return Bool(op(l_formula.n, r_formula.n)), pathcond, avs
+                else:
+                    return BinFExpr(l_formula, r_formula, op_name), pathcond, avs
+            return symb_interpN([l, r], env, pathcond, avs, k)
+        case BoolOp(l, r, op, op_name):
+            # lv = interp(l, env)
+            # rv = interp(r, env)
+            # assert isinstance(lv, Bool) and isinstance(rv, Bool), f"Bad argument types for {op_name}: {lv} and {rv}"
+            # return Bool(op(lv.b, rv.b))
+            def k(formulas, pathcond, avs):
+                l_formula, r_formula = formulas
+                if isinstance(l_formula, Bool) and isinstance(r_formula, Bool):
+                    return Bool(op(l_formula.b, r_formula.b)), pathcond, avs
+                else:
+                    return BinFExpr(l_formula, r_formula, op_name), pathcond, avs
+            return symb_interpN([l, r], env, pathcond, avs, k)
+
+        case Assert(tst):
+            # tst_v = interp(tst, env)
+            # assert isinstance(tst_v, Bool)
+            # if not tst_v.b:
+            #     raise AssertionFailure(f"Assertion failed: {e}")
+            # return tst_v
+
+            # Plan: look at tst, if it could fail, add it to the assertion violations we've found
+
+            # Fun([a, b],
+            #   Let(x, Assert(b > 0),
+            #       Let(y, Assert(a > 5),
+            #           Assert(a + b > -1))))
+            # --> implicit branching! The last assertion actually can't fail, because the prior asserts must have passed
+            # solution: path conditions = all of the conditions under which we get to a certain point in the code
+
+            formula, pc2, avs2 = symb_interp(tst, env, pathcond, avs)
+            if satisfiable(pc2 + [NegFExpr(formula)]):
+                # NOTE difference: The assertion could fail, but any code after this will only run if it passed, so add it to the path condition
+                return formula, pc2 + [formula], avs2 + [(pc2 + [NegFExpr(formula)])]
+            else:
+                # The assertion must succeed
+                return formula, pc2 + [formula], avs2
+
+
+        case _:
+            raise Exception(f"Unknown expr {e}")
+
+def satisfiable(constraints: 'ConstraintSet') -> bool:
+    varmap = {}
+    s = z3.Solver()
+    s.add(*[formula.make_z3(varmap) for formula in constraints])
+    # NOTE difference: solver might say "dunno", so the right choice is to check against unsat
+    return s.check() != z3.unsat
+
+def get_model(constraints):
+    varmap = {}
+    s = z3.Solver()
+    s.add(*[formula.make_z3(varmap) for formula in constraints])
+    assert s.check() != z3.unsat
+    return s.model()
 
--- tests.py
+++ tests.py
@@ -1,15 +1,36 @@
 import unittest
+import z3
 
 from arith import *
 
+def set_eq(l1, l2):
+    if len(l1) != len(l2):
+        return False
+    for elt in l1:
+        if elt not in l2:
+            return False
+    return True
+
+def make_const(v):
+    if isinstance(v, z3.IntNumRef):
+        return Num(v.as_long())
+    elif isinstance(v, z3.BoolRef):
+        return Bool(str(v) == "True")
+    elif isinstance(v, z3.StringRef):
+        return Str(v.as_string())
+def arbitrary_const(typ):
+    match typ:
+        case BaseType.INT:
+            return Num(1000)
+        case BaseType.BOOL:
+            return Bool(False)
+
 class TestInterp(unittest.TestCase):
-    def test_thing(self):
+    def test_concrete(self):
         self.assertEqual(interp(Num(5), {}),
                          Num(5))
-    def test_thing2(self):
         self.assertEqual(interp(Add(Num(5), Num(-1)), {}),
                          Num(4))
-    def test_thing3(self):
         self.assertEqual(interp(Let("x", BaseType.INT,
                                     Num(0),
                                     Mul(Var("x"), Num(2))),
@@ -47,6 +68,242 @@ class TestInterp(unittest.TestCase):
                                         [Num(7)])),
                                 {}),
                          Num(12))
+
+
+        # --- bools, assert ---
+        self.assertEqual(interp(Eq(Num(7), Num(5)),
+                                {}),
+                         Bool(False))
+        self.assertEqual(interp(Lt(Num(2), Num(5)),
+                                {}),
+                         Bool(True))
+        self.assertEqual(interp(Assert(Lt(Num(2), Num(5))),
+                                {}),
+                         Bool(True))
+        with self.assertRaises(AssertionFailure):
+            interp(Assert(Not(Lt(Num(2), Num(5)))),
+                   {})
+
+
+    def test_symb_interp(self):
+        def symb_interp_t(e):
+            f, _, _ = symb_interp(e, {}, [], [])
+            return f
+        self.assertEqual(symb_interp_t(Num(5)),
+                         Num(5))
+        self.assertEqual(symb_interp_t(Add(Num(5), Num(-1))),
+                         Num(4))
+        self.assertEqual(symb_interp_t(Let("x", BaseType.INT,
+                                    Num(0),
+                                    Mul(Var("x"), Num(2)))),
+                         Num(0))
+        self.assertEqual(symb_interp_t(
+            Let("f", FunType([BaseType.INT, BaseType.INT],
+                             BaseType.INT),
+                Fun(["x", "y"],
+                    [BaseType.INT, BaseType.INT],
+                    Add(Var("x"), Var("y"))),
+                App(Var("f"),
+                    [Num(5), Add(Num(2), Num(3))]))),
+
+                         interp(Mul(Num(2), Num(5)), {}))
+
+        self.assertEqual(symb_interp_t(App(Let("x", BaseType.INT, Num(5),
+                                        Fun(["y"], [BaseType.INT],
+                                            Add(Var("x"), Var("y")))),
+                                    [Num(7)])),
+
+                         Num(12))
+        self.assertEqual(symb_interp_t(Let("x", BaseType.INT, Num(100),
+                                    App(Let("x", BaseType.INT, Num(5),
+                                            Fun(["y"], [BaseType.INT],
+                                                Add(Var("x"), Var("y")))),
+                                        [Num(7)]))),
+
+                         Num(12))
+
+
+
+
+
+    def assertSetEq(self, actual, expected, msg=None):
+        if not set_eq(actual, expected):
+            self.fail(self._formatMessage(msg, f"Assertion violation sets differ:\nactual:   {actual!r}\nvs\nexpected: {expected!r}"))
+
+    # symb_exec `fun`, check that it identifies `num_avs` assertion violation path conditions,
+    # and check that executing `fun` with the inputs the SMT solver provides for those
+    # path conditions actually causes an assertion violation
+    def symb_exec_and_check_avs(self, fun, num_avs):
+        f, avs = symb_exec(fun)
+        self.assertEqual(len(avs),
+                         num_avs,
+                         f"Expected {num_avs} assertion violations but got {avs}")
+        for av_path in avs:
+            model = get_model(av_path)
+            param_to_type = {name: typ for name, typ in zip(fun.params, fun.anns)}
+            param_to_val = {str(var): make_const(model[var]) for var in model}
+            with self.assertRaises(AssertionFailure,
+                                   msg=f"Counter-example {model} does not cause an assertion failure!"):
+                interp(App(fun,
+                           [(param_to_val[name] if name in param_to_val \
+                             else arbitrary_const(param_to_type[name])) \
+                            for name in fun.params]),
+                       {})
+        return f, avs
+
+
+
+    def test_symb_exec(self):
+        self.assertEqual(symb_exec_v1(
+            Fun(["a", "b", "c"],
+                [BaseType.INT, BaseType.INT, BaseType.INT],
+                Let("f", FunType([BaseType.INT, BaseType.INT],
+                                 BaseType.INT),
+                    Fun(["x", "y"],
+                        [BaseType.INT, BaseType.INT],
+                        Add(Var("x"), Var("y"))),
+                    App(Var("f"),
+                        [Var("a"), Add(Var("b"), Var("c"))])))),
+
+                         # a + (b + c)
+                         BinFExpr(SymVar("a", BaseType.INT),
+                                  BinFExpr(SymVar("b", BaseType.INT), SymVar("c", BaseType.INT), '+'),
+                                  '+'))
+
+        self.assertEqual(symb_exec_v1(
+            Fun(["a"],
+                [BaseType.INT],
+                App(Let("x", BaseType.INT, Num(5),
+                        Fun(["y"], [BaseType.INT],
+                            Mul(Var("x"), Var("y")))),
+                    [Var("a")]))),
+
+                         # 5a
+                         BinFExpr(Num(5), SymVar("a", BaseType.INT), '*'))
+        self.assertEqual(symb_exec_v1(
+            Fun(["a"],
+                [BaseType.INT],
+                Let("x", BaseType.INT, Num(100),
+                    App(Let("x", BaseType.INT, Var("a"),
+                            Fun(["y"], [BaseType.INT],
+                                Sub(Var("x"), Var("y")))),
+                        [Num(7)])))),
+
+                         # a - 7
+                         BinFExpr(SymVar("a", BaseType.INT), Num(7), '-'))
+
+        # --- bool ops ---
+        self.assertEqual(symb_exec_v1(
+            Fun(["a"],
+                [BaseType.INT],
+                Eq(Num(7), Var("a")))),
+
+                         BinFExpr(Num(7), SymVar("a", BaseType.INT), '='))
+        self.assertEqual(symb_exec_v1(
+            Fun(["a"],
+                [BaseType.BOOL],
+                And(Bool(True), Var("a")))),
+
+                         BinFExpr(Bool(True), SymVar("a", BaseType.BOOL), 'and'))
+
+
+
+
+
+
+
+        # --- assert ---
+        # Note `Seq(e1, e2)` == `Let("dummy", INT, e1, e2)`
+        f, assertion_violation_pathconds = symb_exec(
+            Fun(['x', 'y'],
+                [BaseType.INT, BaseType.INT],
+                BiNumOp(Var('x'), Num(2), lambda x, y: x * y, '*')))
+        self.assertEqual(f, BinFExpr(SymVar('x', BaseType.INT), Num(2), '*'))
+        self.assertEqual(assertion_violation_pathconds,
+
+                         )
+
+        f, assertion_violation_pathconds = self.symb_exec_and_check_avs(
+            Fun(['x', 'y'],
+                [BaseType.INT, BaseType.INT],
+                Seq(Assert(Eq(Var('x'), Num(0))),
+                    Mul(Var('x'), Num(2)))),
+            1)
+        self.assertEqual(f, BinFExpr(SymVar('x', BaseType.INT), Num(2), '*'))
+        self.assertEqual(assertion_violation_pathconds,
+
+                         )
+
+        f, avs = self.symb_exec_and_check_avs(
+            Fun(['x', 'y'],
+                [BaseType.INT, BaseType.INT],
+                Seq(Assert(Eq(Var('x'), Num(0))),
+                    Let('z', BaseType.INT, Mul(Var('x'), Num(2)),
+                        Assert(Gt(Var('z'), Num(1)))))),
+            2)
+        self.assertSetEq(avs,
+                         )
+
+        f, avs = self.symb_exec_and_check_avs(
+            Fun(['I'],
+                [BaseType.INT],
+                Let(
+                    'f', None, Fun(['x'], [BaseType.INT],
+                                   Seq(Assert(Lt(Var('x'), Num(5))),
+                                       Add(Var('x'), Num(1)))),
+                    App(Var('f'), [Var('I')])
+                )),
+            1)
+        self.assertSetEq(avs,
+                         )
+
+
+
+
+        # --- If ---
+        # f, avs = self.symb_exec_and_check_avs(
+        #     Fun(['a', 'b'],
+        #         [BaseType.INT, BaseType.INT],
+        #         If(Lt(Var('a'), Var('b')),
+        #            Assert(Neq(Var('a'), Var('b'))),
+        #            Bool(False))),
+        #      )
+        # f, avs = self.symb_exec_and_check_avs(
+        #     Fun(['a', 'b'],
+        #         [BaseType.INT, BaseType.INT],
+        #         If(Lt(Var('a'), Var('b')),
+        #            Bool(False),
+        #            Assert(Neq(Var('a'), Var('b'))))),
+        #      )
+        # f, avs = self.symb_exec_and_check_avs(
+        #     Fun(['a', 'b'],
+        #         [BaseType.INT, BaseType.INT],
+        #         If(Lt(Var('a'), Var('b')),
+        #            If(Eq(Var('a'), Var('b')),
+        #               Assert(Eq(Var('a'), Num(0))),
+        #               Bool(False)),
+        #            Bool(False))),
+        #      )
+        # f, avs = self.symb_exec_and_check_avs(
+        #     Fun(['a', 'b'],
+        #         [BaseType.INT, BaseType.INT],
+        #         If(Lt(Var('a'), Var('b')),
+        #            If(Eq(Var('a'), Var('b')),
+        #               Bool(False),
+        #               Assert(Eq(Var('a'), Num(0)))),
+        #            Bool(False))),
+        #      )
+        # f, avs = self.symb_exec_and_check_avs(
+        #     Fun(['a', 'b'],
+        #         [BaseType.INT, BaseType.INT],
+        #         Seq(If(Lt(Var('a'), Var('b')),
+        #                Assert(Eq(Var('b'), Num(0))),
+        #                Bool(False)),
+        #             If(Gt(Var('b'), Num(5)),
+        #                Bool(False),
+        #                Assert(Eq(Var('a'), Num(5)))))),
+        #      )
+
 
 if __name__ == "__main__":
     unittest.main()
